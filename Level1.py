c87ee545f2ff7bca3968a5fa2339ca13482ba40f
binary1985 <joshua.alan.platz@gmail.com>
2021-05-24 09:53:39 -0500

    Update Level1.py

--- Level1.py
+++ Level1.py
@@ -8,27 +8,3 @@ with open("rockyou.txt", "rb") as a_file: # Open the file read only in binary fo
     if testhash.hexdigest() == hash: #Compare MD5 hash to target hash
       print ("Found: " + hash + ":" + line.decode('ascii').strip()) # Print results
       break # Quit so we dont process the whole file
-
-
-
-
-# Level 2 Ideas
-
-# Read in a hashlist as Sysarg
-# Read in the word list as sysarg
-
-# Level 3 Ideas
-# Brute force an Algorithm
-
-# Going Further?
-# Multi Thread
-# Build a Pot File
-# Generate Some Rules
-# Optimize Code
-
-
-Syntax Hints
-Make sure you open the wordlist in binary read mode
-hashlib.md5(BINARY_VARIABLE) will give you a hashresult
-hashresult.hexdigest() will give you a HEX Hash
-Compare that hash against your target hash
